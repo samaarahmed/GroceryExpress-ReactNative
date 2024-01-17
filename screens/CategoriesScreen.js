073db98155f9b24a6bd89c588071e9e8866deebd
Samaar Ahmed <diamantbilvard@gmail.com>
2024-01-17 14:52:54 +0100

    Add app setup and dummy data

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+
+function CategoriesScreen(props) {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default CategoriesScreen;
